8a3c3d598d125d1d3353925eb47a8a7645f92e1f
HYUNJUNE <june2ac4dev@gmail.com>
2026-02-13 13:42:15 +0900

    feat:커스텀 모달 추가

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { TextModalProvider } from './src/components/TextModalProvider';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MainScreen from './src/screens/MainScreen';
@@ -11,35 +12,37 @@ const Tab = createBottomTabNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          tabBarActiveTintColor:  '#AABC99', //'#5C9A87',// 활성 아이콘 색상
-          tabBarInactiveTintColor: 'gray', // 비활성 아이콘 색상
-        }}
-      >
-        <Tab.Screen
-          name="Home"
-          component={MainScreen}
-          options={{
-            headerShown: false,
-            tabBarIcon: ({ color, size }) => (
-              <AntDesign name="home" size={size} color={color} />
-            ),
+    <TextModalProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            tabBarActiveTintColor:  '#AABC99', //'#5C9A87',// 활성 아이콘 색상
+            tabBarInactiveTintColor: 'gray', // 비활성 아이콘 색상
           }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
-            title: '프로필',
-            tabBarIcon: ({ color, size }) => (
-              <Octicons name="person" size={size} color={color} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+        >
+          <Tab.Screen
+            name="Home"
+            component={MainScreen}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({ color, size }) => (
+                <AntDesign name="home" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{
+              title: '프로필',
+              tabBarIcon: ({ color, size }) => (
+                <Octicons name="person" size={size} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </TextModalProvider>
   );
 }
--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -2,47 +2,53 @@ import { useRef, useCallback, useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { WebView } from 'react-native-webview';
 import { BackHandler, Platform } from 'react-native';
-import { Alert } from 'react-native';
+import useTextModal from '../components/useTextModal';
 
 interface MainScreenProps {
   navigation: any;
 }
 
 const MainScreen = ({ navigation }: MainScreenProps) => {
-  // WebView의 ref를 생성하고 타입을 지정합니다.
   const webViewRef = useRef<WebView>(null);
-  // WebView가 뒤로 갈 수 있는지 상태를 저장합니다.
-  const [canGoBack, setCanGoBack] = useState(false);
+  const canGoBack = useRef(false);
+  const { showAlert } = useTextModal();
 
   // 안드로이드에서 시스템 백버튼을 눌렀을 때 동작을 정의합니다.
   useEffect(() => {
     if (Platform.OS === 'android') {
       // 백버튼 이벤트 핸들러
       const onBackPress = () => {
-        if (!canGoBack) {
-          Alert.alert('제목', '메시지', [
-            { text: '확인', onPress: () => BackHandler.exitApp() },
-            { text: '취소', style: 'cancel' }
-          ]);
-          return true
-        }
-
         // WebView가 뒤로 갈 수 있으면 goBack() 호출
-        if (canGoBack && webViewRef.current) {
+        if (canGoBack.current && webViewRef.current) {
           webViewRef.current.goBack();
           return true; // 기본 동작(앱 종료) 방지
+        }
+
+        if (!canGoBack.current) {
+          showAlert({
+            title: '제목',
+            message: '메시지',
+            confirmText: '확인',
+            onConfirmAction: () => {
+              BackHandler.exitApp();
+            },
+            cancelText: '취소',
+            onCancelAction: () => {},
+          });
+          return true;
         }
 
         // WebView가 더 이상 뒤로 갈 수 없으면 앱 종료 허용
         return false;
       };
       const backHandler = BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
       // 컴포넌트 언마운트 시 이벤트 제거
       return () => {
         backHandler.remove();
       };
     }
-  }, [canGoBack]);
+  }, []);
 
   return (
     <SafeAreaView style={{ flex: 1 }} edges={["top"]}>
@@ -51,7 +57,9 @@ const MainScreen = ({ navigation }: MainScreenProps) => {
         source={{ uri: 'http://127.0.0.1:5173' }}
         style={{ flex: 1 }}
         //웹페이지를 이동할 때마다 onNavigationStateChange 호출
-        onNavigationStateChange={navState => setCanGoBack(navState.canGoBack)}
+        onNavigationStateChange={navState =>
+          canGoBack.current = navState.canGoBack
+        }
       />
     </SafeAreaView>
   );
